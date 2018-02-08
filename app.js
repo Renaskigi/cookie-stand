48877b30669484b16f60606b14d7a487faa0f568
Ryan Lee <Renaskigi@protonmail.com>
2018-02-08 14:24:45 -0800

    finished adding table

--- app.js
+++ app.js
@@ -2,26 +2,27 @@
 
 const times = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total'];
 
+const form = document.querySelector('form');
+
 //Create Store Objects for Daily Sales Projections
-function Store (name, minHrlyCust,maxHrlyCust,aveCookiesPerCust,elementID) {
+function Store (name, minHrlyCust,maxHrlyCust,aveCookiesPerCust) {
     this.name = name;
     this.minHrlyCust = minHrlyCust;
     this.maxHrlyCust = maxHrlyCust;
     this.aveCookiesPerCust = aveCookiesPerCust;
     this.storeCookiesPerDay = [];
     this.totalSales = 0;
-    this.elementID = elementID;
 }
 
-const pDXAirport = new Store ('PDX Airport', 23, 65, 6.3, 'pdx');
+const pDXAirport = new Store ('PDX Airport', 23, 65, 6.3);
 
-const pioneerSquare = new Store ('Pioneer Square', 3, 24, 1.2, 'pioneer');
+const pioneerSquare = new Store ('Pioneer Square', 3, 24, 1.2);
 
-const powells = new Store ('Powells', 11, 38, 3.7, 'powells');
+const powells = new Store ('Powells', 11, 38, 3.7);
 
-const stJohns = new Store ('St. John\'s', 20, 38, 2.4, 'stjohns');
+const stJohns = new Store ('St. John\'s', 20, 38, 2.4);
 
-const waterfront = new Store ('Waterfront', 2, 16, 4.6, 'waterfront');
+const waterfront = new Store ('Waterfront', 2, 16, 4.6);
 
 Store.prototype.rndmCustHr = function() {
     const min = Math.ceil(this.minHrlyCust);
@@ -84,33 +85,17 @@ for (let i = 0; i < storeNames.length; i++ ) {
     storeNames[i].rowCreator();
 };
 
-// form.addEventListener('submit', function () {
-//     event.preventDefault();
-//     console.log('consoles on submit!');
-
-//     // x - get the data from the form
-//     const title = document.getElementById('title').value;
-//     console.log('the title they wrote', title);
-//     console.log(this.title.value);
-
-//     const content = this.text.value;
-//     const tags = this.tags;
-//     console.log('content', content);
-//     console.log('tags', tags);
-
-//     // x - create the
-//     const newPost = new Post (title, content);
-//     newPost.publish();
-// });
-
-const form = document.querySelector('form');
-
 form.addEventListener('submit', function () {
     event.preventDefault();
-    const newStoreName = document.getElementById('newStoreName');
-    const minCust = document.getElementById('minCust');
-    const maxCust = document.getElementById('maxCust');
-    const avgCookiesPerCust = document.getElementById('avgCookiesPerCust');
+    const newStoreName = document.getElementById('newStoreName').value;
+    const minCust = document.getElementById('minCust').value;
+    const maxCust = document.getElementById('maxCust').value;
+    const avgCookiesPerCust = document.getElementById('avgCookiesPerCust').value;
     console.log('the new store name is', this.newStoreName.value);
     console.log('the minimum customers is', this.minCust.value);
+    const newStore = new Store (newStoreName, minCust, maxCust, avgCookiesPerCust);
+    newStore.rndmCustHr();
+    newStore.cookiesPerHr();
+    newStore.fifteenhr();
+    newStore.rowCreator();
 });
